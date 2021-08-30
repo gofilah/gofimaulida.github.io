15bde23238baaa9e97305852bc203701b330df5c
gofimaulida <gofimaulida@gmail.com>
2021-08-30 23:14:53 +0700

    first commit

---
+++ application/views/home.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- <link rel="stylesheet" href="https://kit.fontawesome.com/a076d05399.js"> -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.12/typed.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <title>Portfolio</title>
+</head>
+
+<body>
+    <div class="scroll-up-btn">
+        <i class="fa fa-sort-up"></i>
+    </div>
+    <nav class="navbar">
+        <div class="max-width">
+            <div class="logo"><a href="#">Portfo<span>lio.</span></a></div>
+            <ul class="menu">
+                <li><a href="#home" class="menu-btn">Home</a></li>
+                <li><a href="#about" class="menu-btn">About</a></li>
+                <li><a href="#services" class="menu-btn">Services</a></li>
+                <li><a href="#skills" class="menu-btn">Skills</a></li>
+                <li><a href="#teams" class="menu-btn">Teams</a></li>
+                <li><a href="#contact" class="menu-btn">Contact</a></li>
+            </ul>
+            <div class="menu-btn">
+                <i class="fa fa-bars"></i>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Home Section Start -->
+    <section class="home" id="home">
+        <div class="max-width">
+            <div class="home-content">
+                <div class="text-1">Hello, My Name Is</div>
+                <div class="text-2">Gofi Maulida</div>
+                <div class="text-3">And I'm a <span class="typing"></span></div>
+                <a href="#">Hire Me</a>
+            </div>
+        </div>
+    </section>
+
+    <!-- About Section Start -->
+    <section class="about" id="about">
+        <div class="max-width">
+            <h2 class="title">About Me</h2>
+            <div class="about-content">
+                <div class="column left">
+                    <img src="assets/img/profile.jpg" alt="">
+                </div>
+                <div class="column right">
+                    <div class="text">I'm Gofi Maulida and I'm a <span>Programmer</span></div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, ipsum atque dolorum eligendi nisi
+                        amet dicta repellendus obcaecati aperiam nihil quaerat earum sit commodi. Minima aliquid odit
+                        saepe, harum quis dicta quas, voluptates ducimus similique necessitatibus laborum ullam ipsum
+                        beatae consectetur assumenda a? Amet sint saepe adipisci itaque obcaecati, eos cum aliquid omnis
+                        officia illo voluptatem voluptate quod earum quibusdam!</p>
+                    <a href="#">Download CV</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Services Section Start -->
+    <section class="services" id="services">
+        <div class="max-width">
+            <h2 class="title">My Services</h2>
+            <div class="serv-content">
+                <div class="card">
+                    <div class="box">
+                        <i class="fa fa-paint-brush"></i>
+                        <div class="text">Programmer</div>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat totam obcaecati explicabo
+                            consequuntur maiores rem?</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fa fa-address-card"></i>
+                        <div class="text">Advertising</div>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat totam obcaecati explicabo
+                            consequuntur maiores rem?</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fa fa-code"></i>
+                        <div class="text">Apps Design</div>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat totam obcaecati explicabo
+                            consequuntur maiores rem?</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Skill Section Start -->
+    <section class="skills" id="skills">
+        <div class="max-width">
+            <h2 class="title">My Skills</h2>
+            <div class="skills-content">
+                <div class="column left">
+                    <div class="text">My Creative Skills & Experiences.</div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam iure similique sit perspiciatis
+                        repudiandae consequuntur enim, dolorum corrupti numquam beatae, consectetur in, esse fugit unde.
+                        Deserunt saepe officiis ratione nostrum fuga odio voluptatum facilis similique praesentium in
+                        quibusdam tempora molestias maxime quis incidunt odit magnam tempore nulla, cum atque nisi?
+                        Perferendis quaerat quas, odio nihil tempora hic doloremque asperiores explicabo.</p>
+                    <a href="#">Read more</a>
+                </div>
+                <div class="column right">
+                    <div class="bars">
+                        <div class="info">
+                            <span>HTML</span>
+                            <span>90%</span>
+                        </div>
+                        <div class="line html"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>CSS</span>
+                            <span>60%</span>
+                        </div>
+                        <div class="line css"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>JavaScript</span>
+                            <span>50%</span>
+                        </div>
+                        <div class="line js"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>PHP</span>
+                            <span>80%</span>
+                        </div>
+                        <div class="line php"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>MySQL</span>
+                            <span>70%</span>
+                        </div>
+                        <div class="line mysql"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+
+    <!-- Teams Section Start -->
+    <section class="teams" id="teams">
+        <div class="max-width">
+            <h2 class="title">My Teams</h2>
+            <div class="carousel owl-carousel">
+                <div class="card">
+                    <div class="box">
+                        <img src="assets/img/default.jpg" alt="">
+                        <div class="text">Someone Name</div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="assets/img/default.jpg" alt="">
+                        <div class="text">Someone Name</div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="assets/img/default.jpg" alt="">
+                        <div class="text">Someone Name</div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="assets/img/default.jpg" alt="">
+                        <div class="text">Someone Name</div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="assets/img/default.jpg" alt="">
+                        <div class="text">Someone Name</div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Contact Section Start -->
+    <section class="contact" id="contact">
+        <div class="max-width">
+            <h2 class="title">Contact Me</h2>
+            <div class="contact-content">
+                <div class="column left">
+                    <div class="text">Get in Touch</div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quidem iste voluptatibus expedita
+                        pariatur natus. Deserunt aliquid eum sapiente iure?</p>
+                    <div class="icons">
+                        <div class="row">
+                            <i class="fa fa-users"></i>
+                            <div class="info">
+                                <div class="head">Name</div>
+                                <div class="sub-title">Gofi Maulida</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fa fa-address-card"></i>
+                            <div class="info">
+                                <div class="head">Address</div>
+                                <div class="sub-title">Magelang, Indonesia</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fa fa-envelope"></i>
+                            <div class="info">
+                                <div class="head">Email</div>
+                                <div class="sub-title">gofimaulida@gmail.com</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column right">
+                    <div class="text">Message Me</div>
+                    <form action="#">
+                        <div class="fields">
+                            <div class="field name">
+                                <input type="text" placeholder="Name" required>
+                            </div>
+                            <div class="field email">
+                                <input type="email" placeholder="Email" required>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <input type="text" placeholder="Subject" required>
+                        </div>
+                        <div class="field textarea">
+                            <textarea cols="30" rows="10" placeholder="Describe project ..." required></textarea>
+                        </div>
+                        <div class="button">
+                            <button type="submit">Send Message</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer Section Start -->
+    <footer>
+        <span>Created By <a href="#">Bimaaa</a> | <span class="fa fa-copyright"></span> 2021 All Rights
+            Reserved.</span>
+    </footer>
+
+    <script src="assets/js/script.js"></script>
+</body>
+
+</html>
